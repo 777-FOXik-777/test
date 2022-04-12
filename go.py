a87736b6f645b259e2a348e66c62d1f52805c0e0
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-04-12 23:19:47 +0300

    Update go.py

--- go.py
+++ go.py
@@ -1,25 +1,8 @@
-import datetime
+import datetime, time
 
 now = datetime.datetime.now()
 
-print
-print ("Текущая дата и время с использованием метода str:")
-print (str(now))
-
-print (" ")
-print ("Текущая дата и время с использованием атрибутов:")
-print ("Текущий год: %d" % now.year)
-print ("Текущий месяц: %d"% now.month)
-print ("Текущий день: %d" % now.day)
-print ("Текущий час: %d" % now.hour)
-print ("Текущая минута: %d" % now.minute)
-print ("Текущая секунда: %d" % now.second)
-print ("Текущая микросекунда: %d" % now.microsecond)
-
-print
-print ("Текущая дата и время с использованием strftime:")
-print (now.strftime("%d-%m-%Y %H:%M"))
-
-print
-print ("Текущая дата и время с использованием isoformat:")
-print (now.isoformat())
+while True:
+    os.system('clear')
+    print (now.strftime("%d-%m-%Y %H:%M"))
+    time.sleep(3)
