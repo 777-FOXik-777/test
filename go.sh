8fdf488577fe5fb81822de28840ce53a6b8a42ad
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-04-13 23:01:04 +0300

    Update go.sh

--- go.sh
+++ go.sh
@@ -270,13 +270,13 @@ increase_followers
 
 elif [[ $option -eq 2 ]]; then
 printf "\n"
-printf "  \e[1;91m Пока !!\e[0m\n"
+printf "  \e[1;91m До встречи !!\e[0m\n"
 printf "\n"
 exit
 
 else
 
-printf " \e[1;93m \n [!] Не могу найти опцию!\e[0m\n"
+printf " \e[1;93m[!] Invalid Option!\e[0m\n"
 sleep 2
 menu
 
