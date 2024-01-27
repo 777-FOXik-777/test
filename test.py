6564932d41e5774bc537099399a9a1bdf93fa6d5
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-27 16:12:16 +0200

    Update test.py

--- test.py
+++ test.py
@@ -30,17 +30,18 @@ print("Локальный сервер запущен на порту 8000")
 # Добавляем задержку, чтобы сервер успел запуститься и обработать запросы
 time.sleep(5)
 
-# Шаг 3: Запуск Serveo.net
+# Шаг 3: Запуск Serveo.net для тунелирования файла
 
 # Используем оригинальную команду Serveo.net без изменений
 tru_201 = '8000'  # Замените на нужный вам порт
-serveo_command = f'ssh -R 80:localhost:{tru_201} serveo.net -T -n index.html'
+file_to_tunnel = 'index.html'  # Замените на нужный вам файл
+serveo_command = f'ssh -R 80:localhost:{tru_201} serveo.net -T {file_to_tunnel}'
 serveo_process = subprocess.Popen(serveo_command, shell=True, stdout=subprocess.PIPE)
 
 # Получаем public URL из вывода процесса Serveo
 serveo_url = serveo_process.stdout.readline().strip().decode('utf-8').split()[-1]
 
-print(f"Приложение доступно по следующему public URL: {serveo_url}")
+print(f"Файл {file_to_tunnel} доступен по следующему public URL: {serveo_url}")
 
 # Добавляем задержку, чтобы скрипт не завершался сразу
 try:
