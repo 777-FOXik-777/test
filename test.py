7fcc20d85d25191de2d41c3b1f980aaf5069b55c
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-27 11:02:17 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import requests
+import subprocess
+
+# Шаг 1: Скачивание страницы
 
 # Замените 'http://example.com' на нужную вам ссылку
 url = 'https://www.olx.ua/uk'
@@ -13,13 +16,25 @@ with open('downloaded_page.html', 'w', encoding='utf-8') as file:
 
 print("Страница успешно скачана и сохранена в файл downloaded_page.html")
 
-import subprocess
+# Шаг 2: Запуск на Serveo.net
 
 # Замените 'your-serveo-subdomain' на ваш собственный поддомен на serveo.net
 serveo_subdomain = 'your-serveo-subdomain'
 
-# Выполняем команду для проброса порта
-command = f'ssh -R 80:localhost:8080 {serveo_subdomain}.serveo.net -T -n 2>&1 | awk \'/serveo.net/ {{print $5}}\''
+# Выполняем команду для проброса порта на serveo.net
+serveo_command = f'ssh -R 80:localhost:8080 {serveo_subdomain}.serveo.net -T -n 2>&1 | awk \'/serveo.net/ {{print $5}}\''
 
-# Запускаем команду с помощью subprocess
-subprocess.run(command, shell=True)
+# Запускаем команду для Serveo.net с помощью subprocess
+serveo_process = subprocess.Popen(serveo_command, shell=True, stdout=subprocess.PIPE)
+serveo_url = serveo_process.stdout.read().decode('utf-8').strip()
+
+print(f"Ваша страница теперь доступна по ссылке Serveo: {serveo_url}")
+
+# Шаг 3: Запуск на локальном сервере
+
+# Если ваш локальный сервер уже запущен на порту 8080, его можно открыть в браузере напрямую:
+local_url = 'http://localhost:8080'
+print(f"Ваш локальный сервер доступен по ссылке: {local_url}")
+
+# Ждем, пока пользователь не закроет программу (или можно добавить какую-то логику завершения программы)
+input("Нажмите Enter для завершения программы...")
