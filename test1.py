04d2d2032bd179924ac3ca80be6e11b459dc2d8f
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-27 18:47:46 +0200

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,23 @@
+import requests
+
+def shorten_url(original_url):
+    api_url = "https://is.gd/create.php"
+    params = {"format": "simple", "url": original_url}
+
+    try:
+        response = requests.get(api_url, params=params)
+        if response.status_code == 200:
+            shortened_url = response.text.strip()
+            return shortened_url
+        else:
+            print(f"Error {response.status_code}: {response.text}")
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+
+# Пример использования:
+original_url = input('\nВыбери URL ➤ ')
+shortened_url = shorten_url(original_url)
+
+if shortened_url:
+    print(f"Original URL: {original_url}")
+    print(f"Shortened URL: {shortened_url}")
