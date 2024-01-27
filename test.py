f119ce2f4469164ef6acdfd26641378ea44dd9da
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-27 13:20:19 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,38 +1,36 @@
-import requests
-import subprocess
-import time
 import os
+import requests
+from requests_html import HTMLSession
 
 # Шаг 1: Скачивание страницы
 
 # Замените 'http://example.com' на нужную вам ссылку
 url = 'https://www.olx.ua/uk'
 
+url = input('\n Выбери url ➤ ')
+
 # Скачиваем страницу по ссылке
-response = requests.get(url)
-html_content = response.text
+session = HTMLSession()
+response = session.get(url)
+response.html.render()
 
 # Сохраняем HTML-код в файл
 file_path = 'downloaded_page.html'
 with open(file_path, 'w', encoding='utf-8') as file:
-    file.write(html_content)
+    file.write(response.html.html)
 
 print(f"Страница успешно скачана и сохранена в файл {file_path}")
 
 # Шаг 2: Запуск локального сервера
 
-# Копируем содержимое файла в файл index.html в текущей рабочей директории
-with open('index.html', 'w', encoding='utf-8') as file:
-    file.write(html_content)
-
-# Выполняем команду для запуска локального сервера на порту 8080
-local_server_command = 'python -m http.server 8080'
+# Выполняем команду для запуска локального сервера на порту 8000 (или другом свободном порту)
+local_server_command = 'python -m http.server 8000'
 
 # Запускаем команду для локального сервера с помощью subprocess
 local_server_process = subprocess.Popen(local_server_command, shell=True, stdout=subprocess.PIPE)
 
 # Печатаем сообщение о запуске локального сервера
-print("Локальный сервер запущен на порту 8080")
+print("Локальный сервер запущен на порту 8000")
 
 # Добавляем задержку, чтобы скрипт не завершался сразу
 try:
