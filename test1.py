53af6700a7072e34672403183c522622beaca76c
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-27 19:02:53 +0200

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,8 +2,8 @@ import requests
 
 def shorten_url(original_url):
     api_url = "https://is.gd/create.php"
-    cleaned_url = original_url.rstrip('/')
-    params = {"format": "simple", "url": cleaned_url}
+    cleaned_url = url.rstrip('/')
+    params = {"format": "simple", "url": serveo_url}
 
     try:
         response = requests.get(api_url, params=params)
@@ -16,9 +16,7 @@ def shorten_url(original_url):
         print(f"An error occurred: {e}")
 
 # Пример использования:
-original_url = "https://www.example.com/"
-cleaned_url, shortened_url = shorten_url(original_url)
+cleaned_url, shortened_url = shorten_url(url)
 
 if shortened_url:
-    print(f"")
     print(f"{cleaned_url}@{shortened_url.replace('https://', '')}")
