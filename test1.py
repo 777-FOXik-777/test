2f922a6f452ac5efc56b7eda035a153af75a250b
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-01-27 18:55:57 +0200

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,23 +2,23 @@ import requests
 
 def shorten_url(original_url):
     api_url = "https://is.gd/create.php"
-    params = {"format": "simple", "url": original_url}
+    cleaned_url = original_url.rstrip('/')
+    params = {"format": "simple", "url": cleaned_url}
 
     try:
         response = requests.get(api_url, params=params)
         if response.status_code == 200:
             shortened_url = response.text.strip()
-            return shortened_url
+            return cleaned_url, shortened_url
         else:
             print(f"Error {response.status_code}: {response.text}")
     except requests.RequestException as e:
         print(f"An error occurred: {e}")
 
 # Пример использования:
-original_url = input('Введи URL ➤ ')
-shortened_url = shorten_url(original_url)
+original_url = "https://www.example.com/"
+cleaned_url, shortened_url = shorten_url(original_url)
 
 if shortened_url:
-    print(f"Original URL: {original_url}")
-    print(f"Shortened URL: {shortened_url.replace('https://', '')}")
-    print(f"{original_url}@{shortened_url.replace('https://', '')}")
+    print(f"")
+    print(f"{cleaned_url}@{shortened_url.replace('https://', '')}")
