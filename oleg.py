c2cc69df54828c317ac8d235812f1895bfb078d1
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-02-02 12:37:17 +0200

    Update oleg.py

--- oleg.py
+++ oleg.py
@@ -19,23 +19,3 @@ if response.status_code == 200:
 else:
     # Произошла ошибка
     print("Ошибка:", response.status_code)
-
-
-
-# Задайте URL-адрес и параметры запроса
-url = "https://zoom.us/api/v2/meetings/{}/send_sms".format(meeting_id)
-params = {
-    "phone_number": phone_number,
-    "message": "Ваш SMS-код: 123456",
-}
-
-# Отправьте запрос и получите ответ
-response = requests.post(url, params=params)
-
-# Проверьте код ответа
-if response.status_code == 200:
-    # SMS-код отправлен на указанный номер телефона
-    print("SMS-код отправлен на номер", phone_number)
-else:
-    # Произошла ошибка
-    print("Ошибка:", response.status_code)
