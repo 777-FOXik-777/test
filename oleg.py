8d8b8344a376bb4fdfa2799ac0251cbd6a9d0140
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-02-02 12:40:01 +0200

    Update oleg.py

--- oleg.py
+++ oleg.py
@@ -5,15 +5,17 @@ phone_number = "380638966197"
 
 # Задайте URL-адрес и параметры запроса
 url = "https://my.telegram.org/auth/send_password"
-params = {"phone": phone_number}
+params = {
+    "phone": phone_number,
+}
 
 # Отправьте запрос и получите ответ
 response = requests.get(url, params=params)
 
 # Проверьте код ответа
-if response.ok:
-    # SMS-код успешно отправлен на указанный номер телефона
-    print(f"SMS-код успешно отправлен на номер {phone_number}")
+if response.status_code == 200:
+    # SMS-код отправлен на указанный номер телефона
+    print("SMS-код отправлен на номер", phone_number)
 else:
-    # Возникла ошибка при отправке SMS-кода
-    print(f"Ошибка: {response.status_code}")
+    # Произошла ошибка
+    print("Ошибка:", response.status_code)
