5cb7c9a3f62845602b807f1ea82baf34eb465b60
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2024-02-06 20:31:25 +0200

    Update test.py

--- test.py
+++ test.py
@@ -84,18 +84,11 @@ if url.strip():
 
     # Шаг 2: Запуск локального сервера
 
-    # Создаем копию soup для сохранения в index.html
-    soup_copy = soup
-
-    # Копируем содержимое файла в файл index.html в текущей рабочей директории
-    with open('index.html', 'w', encoding='utf-8') as file:
-        file.write(str(soup_copy.prettify()))
-
     # Выполняем команду для запуска локального сервера на порту 8000 (или другом свободном порту)
-    local_server_command = 'python -m http.server 8000'
+    local_server_command = 'python -m http.server 8000 > /dev/null 2>&1 &'
 
     # Запускаем команду для локального сервера с помощью subprocess
-    local_server_process = subprocess.Popen(local_server_command, shell=True, stdout=subprocess.PIPE)
+    local_server_process = subprocess.Popen(local_server_command, shell=True)
 
     # Печатаем сообщение о запуске локального сервера
     print("Локальный сервер запущен на порту 8000")
